ca8ebb7a6ce3474fe909c2698d57ee59ac311566
nhokphal <68092827+nhokphal@users.noreply.github.com>
2022-01-27 16:49:48 +0700

    object oriented in python with class

---
+++ classes.py
@@ -0,0 +1,11 @@
+
+# define class
+class Point():
+    def __init__ (self, input1, input2): # define 
+        self.x = input1#store data inside self similar to "this" in javascript
+        self.y = input2  # 
+
+
+p = Point(20, 8) # here we adding value 
+print(p.x)
+print(p.y)

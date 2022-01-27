ca527453c3790e27089f8ff15ea319a0750740d6
nhokphal <68092827+nhokphal@users.noreply.github.com>
2022-01-27 16:38:58 +0700

    import local function in python

---
+++ square.py
@@ -0,0 +1,18 @@
+# import from local function on pc
+
+from function import square # now its import square function from function local   
+
+
+for i in range(10):
+    print(f"the square of {i} is {square(i)}") # loop 10 times, each time we loop we print something else in the square function above
+
+
+
+
+
+# second deference way approach 
+
+import function 
+
+for i in range(10): 
+    print(f"the square of {i} is {function.square(i)} value") # calculate itself values 

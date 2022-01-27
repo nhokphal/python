f45e3dac804eeb283edf4c4227b65d2e3fb1954d
nhokphal <68092827+nhokphal@users.noreply.github.com>
2022-01-27 16:21:33 +0700

    define loop in python

---
+++ loop.py
@@ -0,0 +1,22 @@
+# define loop
+
+
+for i in [1, 2, 3, 4, 5]:
+    print(i) # print out value in the loop
+
+
+for i in range(6): # mean get me range from 0-5
+    print(i)
+
+# define name in the array as loop
+names = ["phal", "dara", "joker", "nerd"]
+
+for name in names:
+    print(name) # print out all the list in the array 
+
+
+# okey now we use string in loop
+name = "phal"
+
+for character in name:
+    print(character)# out put p h a l, each number of letter 

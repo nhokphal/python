0b2242b6c6dbc770664a3c6dfc60b069f32d447f
nhokphal <68092827+nhokphal@users.noreply.github.com>
2022-01-27 16:14:45 +0700

    defince set in python

---
+++ set.py
@@ -0,0 +1,20 @@
+#create an empty set
+
+s = set()
+
+# add elements to set   
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(4) # not print number 4 twice 
+print(s)
+
+
+# remove
+s.remove(2)
+print(s)
+
+#len give number similar .length in javascript
+
+print(f"the set has {len(s)} in the site.") # show number in the define (s) variable

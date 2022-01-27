4350389020fc57f2c6c4b311fa85dde656ee3a18
nhokphal <68092827+nhokphal@users.noreply.github.com>
2022-01-27 16:32:17 +0700

    define function

---
+++ function.py
@@ -0,0 +1,8 @@
+# define "def it means define" function in py
+
+def square(x):
+    return x * x # multiply by itself
+
+
+for i in range(10):
+    print(f"the square of {i} is {square(i)}") # loop 10 times, each time we loop we print something else in the square function above
